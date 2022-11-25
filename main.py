7f88a34b1c09fc570aa2fe27539f0aa0bbe4049b
rt3310 <btac3310@gmail.com>
2022-11-25 18:58:50 +0900

    feat: upload resnet34 algorithm

---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+extend_in = np.zeros([1, 2])
+print(extend_in)
---
+++ train.py
@@ -0,0 +1,46 @@
+from data.DataLoader import DataLoader
+from model.ResNet34 import ResNet34
+from test import test
+from trainer.Trainer import Trainer
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    batch_size = 8
+    image_h = 64
+    image_w = 64
+    dataset = DataLoader("./resources/dataset/digit_data", "train_data.txt", batch_size, image_h, image_w)
+
+    model = ResNet34(20)
+
+    init_lr = 0.01
+    train = Trainer(model, dataset, 20, init_lr)
+    loss = []
+    accurate = []
+    temp = 0
+
+    model.train()
+    plt.figure(figsize=(10, 5))
+    plt.ion()
+    for i in range(25000):
+        temp += train.iterate()
+        if i % 10 == 0 and i != 0:
+            loss.append(temp / 10)
+            print("iteration = {} || loss = {}".format(str(i), str(temp/10)))
+            temp = 0
+            if i % 100 == 0:
+                model.eval()
+                accurate.append(test(model, "test.txt", image_h, image_w))
+                model.save("model2")
+                model.train()
+        plt.cla()
+        plt.subplot(1, 2, 1)
+        plt.plot(loss)
+        plt.subplot(1, 2, 2)
+        plt.plot(accurate)
+        plt.pause(0.1)
+
+        if i == 15000:
+            train.set_lr(0.001)
+
+    plt.ioff()
+    plt.show()
